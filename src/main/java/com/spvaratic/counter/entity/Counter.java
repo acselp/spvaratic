e86f63f22d2dd847d145eb6ad2c5aeb64154e210
Verj <virgiliu.plesca@iis.utm.md>
2022-11-14 13:47:12 +0200

    added develop branch

--- src/main/java/com/spvaratic/counter/entity/Counter.java
+++ src/main/java/com/spvaratic/counter/entity/Counter.java
@@ -1,7 +1,23 @@
 package com.spvaratic.counter.entity;
 
+import lombok.Data;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
+@Data
 public class Counter {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int idClient;
+    private int apaCurent;
+    private int salubrizareCurent;
+    @Column(nullable = false,
+            columnDefinition="TIMESTAMP default CURRENT_TIMESTAMP")
+    private java.sql.Timestamp addedOn;
+
 }
